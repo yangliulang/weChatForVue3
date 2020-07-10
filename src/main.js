e66d22c307267cce1553ddb197656ea3492d4398
杨永 <yongyang@58btc.com>
2020-07-10 11:39:53 +0800

    dq

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "mobile-reset.css";
-import "./assets/styles/index.less";
 createApp(App)
   .use(router)
   .use(store)
